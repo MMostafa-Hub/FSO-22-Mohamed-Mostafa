85190990aa9566bafd733d53a29b353fd669ca40
MMostafa-Hub <mmostafa.ibra21@gmail.com>
2022-09-22 23:21:55 +0200

    1.7 and 1.8: unicafe step3 and 4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,12 +16,10 @@ const Stat = ({ variable, value }) => {
     </p>
   );
 };
-export default function App() {
-  const [goodCounter, setGoodCounter] = useState(0);
-  const [neutralCounter, setNeutralCounter] = useState(0);
-  const [badCounter, setBadCounter] = useState(0);
-
+const ShowStatistics = ({ goodCounter, neutralCounter, badCounter }) => {
   let totalFeedbacks = goodCounter + neutralCounter + badCounter;
+  // calculates the average feedback base on these weights
+  // (good: 1, neutral: 0, bad: -1)
   const calculateAverageFeedback = (
     goodCounter,
     badCounter,
@@ -30,6 +28,39 @@ export default function App() {
     return (goodCounter + badCounter * -1) / totalFeedbacks;
   };
 
+  // returns a string representation of the percentage of the good feedback
+  const calculatePositivePercentage = (goodCounter, totalFeedbacks) => {
+    return String(100 * (goodCounter / totalFeedbacks)) + "%";
+  };
+  // shows the feedback only if there's a feedback available
+  if (totalFeedbacks) {
+    return (
+      <>
+        <Stat variable="good" value={goodCounter} />
+        <Stat variable="neutral" value={neutralCounter} />
+        <Stat variable="bad" value={badCounter} />
+        <Stat variable="all" value={totalFeedbacks} />
+        <Stat
+          variable="average"
+          value={calculateAverageFeedback(
+            goodCounter,
+            badCounter,
+            totalFeedbacks
+          )}
+        />
+        <Stat
+          variable="positive"
+          value={calculatePositivePercentage(goodCounter, totalFeedbacks)}
+        />
+      </>
+    );
+  }
+  return <p>No feedback Given</p>;
+};
+export default function App() {
+  const [goodCounter, setGoodCounter] = useState(0);
+  const [neutralCounter, setNeutralCounter] = useState(0);
+  const [badCounter, setBadCounter] = useState(0);
   const incrementGoodCounter = () => {
     setGoodCounter(goodCounter + 1);
   };
@@ -39,34 +70,17 @@ export default function App() {
   const incrementBadCounter = () => {
     setBadCounter(badCounter + 1);
   };
-  const calculatePositivePercentage = (goodCounter, totalFeedbacks) => {
-    return String(100 * (goodCounter / totalFeedbacks)) + "%";
-  };
-
   return (
     <>
       <Headline text="give feedback" />
       <Button onClick={incrementGoodCounter} text="good" />
       <Button onClick={incrementNeutralCounter} text="neutral" />
       <Button onClick={incrementBadCounter} text="bad" />
-
       <Headline text="statistics" />
-
-      <Stat variable="good" value={goodCounter} />
-      <Stat variable="neutral" value={neutralCounter} />
-      <Stat variable="bad" value={badCounter} />
-      <Stat variable="all" value={totalFeedbacks} />
-      <Stat
-        variable="average"
-        value={calculateAverageFeedback(
-          goodCounter,
-          badCounter,
-          totalFeedbacks
-        )}
-      />
-      <Stat
-        variable="positive"
-        value={calculatePositivePercentage(goodCounter, totalFeedbacks)}
+      <ShowStatistics
+        goodCounter={goodCounter}
+        neutralCounter={neutralCounter}
+        badCounter={badCounter}
       />
     </>
   );
