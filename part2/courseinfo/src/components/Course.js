928d81f31d76a5790d2f07426ed32c6e4f618e3a
MMostafa-Hub <mmostafa.ibra21@gmail.com>
2022-09-24 22:34:38 +0200

    2.2: Course information step7

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,37 +1,37 @@
 import React from "react";
 
 // takes care of rendering the title of the course
-const Title = ({ courseTitle }) => {
-  return <h1>{courseTitle}</h1>;
-};
+const Title = ({ courseTitle }) => <h1>{courseTitle}</h1>;
 
-// takes care of rendering the name of the Part
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
+// takes care of rendering the name and the number of exercises of the Part
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+// renders all the parts and their number of exercises
+const Content = ({ courseParts }) => (
+  <div>
+    {courseParts.map((coursePart, index) => (
+      <Part key={index} part={coursePart} />
+    ))}
+  </div>
+);
+
+const TotalNumberOfExercises = ({ courseParts }) => (
+  <>
+    <p style={{ fontWeight: "bold" }}>
+      total of {courseParts.reduce((sum, x) => sum + x.exercises, 0)} exercises
     </p>
-  );
-};
-
-// renders the parts and their number of exercises
-const Content = ({ courseParts }) => {
-  return (
-    <div>
-      {courseParts.map((coursePart, index) => (
-        <Part key={index} part={coursePart} />
-      ))}
-    </div>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Title courseTitle={course.name} />
-      <Content courseParts={course.parts} />
-    </>
-  );
-};
+  </>
+);
+const Course = ({ course }) => (
+  <>
+    <Title courseTitle={course.name} />
+    <Content courseParts={course.parts} />
+    <TotalNumberOfExercises courseParts={course.parts} />
+  </>
+);
 
 export default Course;
