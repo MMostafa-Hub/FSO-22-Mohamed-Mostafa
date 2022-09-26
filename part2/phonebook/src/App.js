d68b89536b548ff0867ce312180185f3a1f4bebf
MMostafa-Hub <mmostafa.ibra21@gmail.com>
2022-09-26 23:43:39 +0200

    2.10 Refactoring phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
-
-// Person object equality tests
-const isSamePerson = (object, other) =>
-  object.name === other.name && object.number === other.number;
+import AddPersonForm from "./components/AddPersonForm";
+import ShowPersons from "./components/ShowPersons";
+import SearchForm from "./components/SearchForm";
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -17,73 +16,28 @@ const App = () => {
   const [PersonsToShow, setPersonsToShow] = useState(persons);
   const [personSearch, setPersonSearch] = useState("");
 
-  const handleDataChange = (set) => (event) => {
-    set(event.target.value);
-  };
-
-  // event handle for submitting the form
-  const addPerson = (event) => {
-    event.preventDefault();
-
-    const newPerson = { name: newName, number: newNumber };
-
-    // Checking if the user already submitted the same person
-    if (persons.find((person) => isSamePerson(person, newPerson))) {
-      alert(`${newName} is already added to phonebook`);
-      return;
-    }
-
-    setNewName("");
-    setNewNumber("");
-    let concatPersonsList = persons.concat(newPerson);
-    setPersons(concatPersonsList);
-    setPersonsToShow(concatPersonsList);
-  };
-
-  const searchPhoneBook = (event) => {
-    event.preventDefault();
-
-    let personName = event.target.value;
-    if (personName === "") setPersonsToShow(persons);
-    else {
-      setPersonsToShow(
-        persons.filter((persons) => persons.name === personName)
-      );
-    }
-    setPersonSearch(personName);
-  };
   return (
     <>
       <h2>Phonebook</h2>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <p>filter shown with</p>
-        <input value={personSearch} onChange={searchPhoneBook} />
-      </form>
+      <SearchForm
+        personSearch={personSearch}
+        setPersonSearch={setPersonSearch}
+        setPersonsToShow={setPersonsToShow}
+        persons={persons}
+      />
       <h2>Add New Person</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name:{" "}
-          <input value={newName} onChange={handleDataChange(setNewName)} />
-        </div>
-        <div>
-          number:{" "}
-          <input value={newNumber} onChange={handleDataChange(setNewNumber)} />
-        </div>
-        <button type="submit">Add</button>
-      </form>
+      <AddPersonForm
+        persons={persons}
+        setPersons={setPersons}
+        setPersonsToShow={setPersonsToShow}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+      />
 
       <h2>Numbers</h2>
-      <div>
-        {PersonsToShow.map((person, index) => (
-          <p key={index}>
-            {person.name} {person.number}
-          </p>
-        ))}
-      </div>
+      <ShowPersons PersonsToShow={PersonsToShow} />
     </>
   );
 };
---
+++ part2/phonebook/src/components/SearchForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const SearchForm = ({
+  personSearch,
+  setPersonSearch,
+  setPersonsToShow,
+  persons,
+}) => {
+  const searchPhoneBook = (event) => {
+    event.preventDefault();
+
+    let personName = event.target.value;
+    if (personName === "") setPersonsToShow(persons);
+    else {
+      setPersonsToShow(persons.filter((person) => person.name === personName));
+    }
+    setPersonSearch(personName);
+  };
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      <p>filter shown with</p>
+      <input value={personSearch} onChange={searchPhoneBook} />
+    </form>
+  );
+};
+
+export default SearchForm;
