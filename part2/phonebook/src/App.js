7d3ad83524c7a187da6d3aa4de31771ac205a9c4
MMostafa-Hub <mmostafa.ibra21@gmail.com>
2022-09-26 22:56:00 +0200

    2.9 phonbook search

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,36 +1,87 @@
 import { useState } from "react";
 
-const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+// Person object equality tests
+const isSamePerson = (object, other) =>
+  object.name === other.name && object.number === other.number;
 
-  // event handler for typing data in the input field
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [PersonsToShow, setPersonsToShow] = useState(persons);
+  const [personSearch, setPersonSearch] = useState("");
+
+  const handleDataChange = (set) => (event) => {
+    set(event.target.value);
   };
 
   // event handle for submitting the form
   const addPerson = (event) => {
     event.preventDefault();
 
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
+
+    // Checking if the user already submitted the same person
+    if (persons.find((person) => isSamePerson(person, newPerson))) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
 
     setNewName("");
-    setPersons(persons.concat(newPerson));
+    setNewNumber("");
+    let concatPersonsList = persons.concat(newPerson);
+    setPersons(concatPersonsList);
+    setPersonsToShow(concatPersonsList);
   };
 
+  const searchPhoneBook = (event) => {
+    event.preventDefault();
+
+    let personName = event.target.value;
+    if (personName === "") setPersonsToShow(persons);
+    else {
+      setPersonsToShow(
+        persons.filter((persons) => persons.name === personName)
+      );
+    }
+    setPersonSearch(personName);
+  };
   return (
     <>
       <h2>Phonebook</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <p>filter shown with</p>
+        <input value={personSearch} onChange={searchPhoneBook} />
+      </form>
+      <h2>Add New Person</h2>
       <form onSubmit={addPerson}>
-        name: <input value={newName} onChange={handleNameChange} />
+        <div>
+          name:{" "}
+          <input value={newName} onChange={handleDataChange(setNewName)} />
+        </div>
+        <div>
+          number:{" "}
+          <input value={newNumber} onChange={handleDataChange(setNewNumber)} />
+        </div>
         <button type="submit">Add</button>
       </form>
 
       <h2>Numbers</h2>
       <div>
-        {persons.map((person, index) => (
-          <p key={index}>{person.name}</p>
+        {PersonsToShow.map((person, index) => (
+          <p key={index}>
+            {person.name} {person.number}
+          </p>
         ))}
       </div>
     </>
